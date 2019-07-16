aa0540fe8145f1866d1af035d3322ca9b7f12066
Bill Dinger <bill.dinger@vml.com>
2019-07-15 23:39:01 -0500

    added some more demo code for performance API & monitoring.

---
+++ measure/measure_basic.js
@@ -0,0 +1,13 @@
+const registerPerformance = function(markName) {
+  const endName = `END_${markName}`;
+  const measureName = `MEASURE_${markName}`;
+  window.performance.mark(markName);
+
+  return () => {
+    window.performance.mark(endName);
+    window.performance.measure(measureName, markName, endName);
+    console.log(window.performance.getEntriesByName(measureName, 'measure'));
+  };
+};
+
+
---
+++ measure/work.js
@@ -0,0 +1,50 @@
+const results = function(caller, message) {
+  const done = registerPerformance('createDomNode');
+  const time = new Date().toLocaleTimeString();
+  const p = document.createElement('p');
+  p.appendChild(document.createTextNode(`${time} [${caller}] - ${message}`));
+  const ele = document.getElementById('results');
+  ele.appendChild(p);
+  done();
+};
+
+const networkWork = function(async) {
+  const done = registerPerformance(`networkWork_async_${async}`);
+  const request = new XMLHttpRequest();
+  request.onloadend = () => {
+    results('networkWork', 'Non Blocking complete!');
+    done();
+  };
+  request.onloadstart = () => {
+    results('networkWork', `Starting a async: ${async} HTTP Request.`);
+  };
+  request.open('GET', 'https://jsonplaceholder.typicode.com/comments', async);
+  request.send();
+};
+
+(function() {
+  const commentsRunning = registerPerformance('comments running');
+  const jsonResult = fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(function(response) {
+        results('fetch', 'retrieved the JSON results.');
+        const result = response.json();
+        results('fetch', 'finished parsing the json.');
+        return result;
+      });
+  commentsRunning();
+
+  // 1. download some data.
+  networkWork(true, 'https://jsonplaceholder.typicode.com/comments');
+
+  // 2.) insert some data into the dom.
+  jsonResult.then((data) => {
+    const json = registerPerformance('json');
+    const resultsEle = document.getElementById('fetchresults');
+    data.forEach((comment) => {
+      const item = document.createElement('p');
+      item.innerHTML = `Name: ${comment.name} Email: ${comment.email}`;
+      resultsEle.appendChild(item);
+    });
+    json();
+  });
+})();

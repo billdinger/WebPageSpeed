6bb05d6c4e97f34b0d845495b68c46d379a0a3da
Bill Dinger <William.Dinger@ameritas.com>
2019-11-22 11:25:32 -0700

    Adding in JSDOC

--- src/measure/work.js
+++ src/measure/work.js
@@ -1,3 +1,8 @@
+
+/** This function handles the API respones.
+ * @param  {string} caller the name of the calling function
+ * @param  {object} message the object to display to the dom
+ */
 const results = function(caller, message) {
   const done = registerPerformance('createDomNode');
   const time = new Date().toLocaleTimeString();
@@ -8,8 +13,20 @@ const results = function(caller, message) {
   done();
 };
 
+/** Performs a network call either sync or async depending on param.
+ * @param  {boolean} async true to make this async, false to make it sync.
+ */
 const networkWork = function(async) {
+  // 1.) Register our performance marker.
   const done = registerPerformance(`networkWork_async_${async}`);
+
+  // 2.) Wipe out existing dom.
+  const ele = document.getElementById('results');
+  while (ele.firstChild) {
+    ele.firstChild.remove();
+  };
+
+  // 3.) Create our request + handler.
   const request = new XMLHttpRequest();
   request.onloadend = () => {
     results('networkWork', 'Non Blocking complete!');
@@ -18,7 +35,7 @@ const networkWork = function(async) {
   request.onloadstart = () => {
     results('networkWork', `Starting a async: ${async} HTTP Request.`);
   };
-  request.open('GET', 'https://jsonplaceholder.typicode.com/comments', false);
+  request.open('GET', 'https://jsonplaceholder.typicode.com/comments', async);
   request.send();
 };
 
@@ -36,10 +53,10 @@ const networkWork = function(async) {
   networkWork(true, 'https://jsonplaceholder.typicode.com/comments');
 
   // 2.) insert some data into the dom.
-  jsonResult.then(data => {
+  jsonResult.then((data) => {
     const json = registerPerformance('json');
     const resultsEle = document.getElementById('fetchresults');
-    data.forEach(comment => {
+    data.forEach((comment) => {
       const item = document.createElement('p');
       item.innerHTML = `Name: ${comment.name} Email: ${comment.email}`;
       resultsEle.appendChild(item);
